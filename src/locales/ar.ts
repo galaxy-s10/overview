4d6c6413d161e3610c6859e9b300bec30a054710
galaxy-s10 <2274751790@qq.com>
2021-09-19 21:05:11 +0800

    0919

---
+++ src/locales/ar.ts
@@ -0,0 +1,53 @@
+/**
+ * 国际化配置文件
+ */
+export default {
+  'button.buyNProducts': 'اشتراء {N} بضائع',
+  'button.save': 'حفظ',
+  'button.payN': 'دفع: {N} ',
+
+  'confirm.youSureCloseIt':
+    'هل أنت متأكد من اغلاقه؟  بعد اغلاق، سوف لا أحد ينظر نموذج 4Fun الخاص بك في أي مكان',
+  'confirm.youSureOpenIt':
+    'هل أنت متأكد من فتحه؟  بعد فتح، يمكن للآخرين أن ينظر نموذج 4Fun الخاص بك',
+  'confirm.noSave': 'لم تحفظ نموذجك 4Fun الذي عدلته، هل أنت متأكد من الخروج؟',
+  'confirm.createModel': 'يرجي إنشاء نموذج 4Fun وفقا لجنسك',
+  'confirm.noEditSex':
+    'لا يمكن تعديل جنسك بعد التأكيد هذه المرة، الرجاء اختيار جنسك الحقيقي.',
+
+  'type.mine': 'الخاص بي',
+
+  'price.free': 'مجانية',
+  'price.total': 'السعر الإجمالي',
+
+  'airbubbles.toDressUp': 'اضغط علي لإرتداء ملابس',
+  'airbubbles.nameModel': 'نموذج 4Fun {اسم}',
+  'airbubbles.createModel': 'اضغط علي لإنشاء نموذج 4Fun الخاص بك',
+
+  'settging.closeSwitch': 'اغلاق نموذج 4Fun',
+  'setting.openSwitch': 'فتح نموذج 4Fun',
+
+  'type.face': 'وجه',
+  'type.eyes': 'عيون',
+  'type.mouth': 'فم',
+  'type.nose': 'أنف',
+  'type.ear': 'آذان',
+  'type.eyebrows': 'حواجب',
+  'type.beard': 'لحية',
+  'type.hair': 'شعر',
+  'type.skinColor': 'لون الجلد',
+  'type.tops': 'ملابس',
+  'type.bottoms': 'سراويل',
+  'type.shones': 'أحذية',
+
+  'validity.forver': 'إلى الأبد',
+
+  'storeState.soldOut': 'بياع بالكامل',
+  'storeState.owned': 'تملكها بالفعل',
+
+  'toast.openSuccess': 'تم إغلاقه بنجاح',
+  'toast.closeSuccess': 'تم فتحه بنجاح',
+  'toast.saveSuccess': 'تم حفظ نموذج 4Fun الحالي',
+  'toast.storeSoldOut':
+    'آه، تم بيع البضاعة التي حددته ، وتم إزالة الماس من هذه البضاعة',
+};
---
+++ src/locales/en.ts
@@ -0,0 +1,54 @@
+/**
+ * 国际化配置文件
+ */
+export default {
+  'button.buyNProducts': 'Buy {N} Products', //
+  'button.save': 'Save',
+  'button.payN': 'Pay {N}', //
+
+  'confirm.youSureCloseIt':
+    'Are you sure to close it? After closing, other people will not see your 4Fun Model anywhere, only you can see it.', //
+  'confirm.youSureOpenIt':
+    'Are you sure to open it? After opening, other people can see your 4Fun Model.', //
+  'confirm.noSave':
+    'Your modified 4Fun Model has not been saved yet, are you sure to exit?', //
+  'confirm.createModel': 'Please set your gender to create 4Fun Model', //
+  'confirm.noEditSex':
+    'Your gender can not be modified after confirmed this time.Please choose your real gender.', //
+
+  'type.mine': 'Mine', //
+
+  'price.free': 'Free',
+  'price.total': 'Total Price', //
+
+  'airbubbles.toDressUp': 'Tap me to dress up', //
+  'airbubbles.nameModel': '{用户名}’s4Fun Model', //
+  'airbubbles.createModel': 'Tap me to create your 4Fun Model', //
+
+  'settging.closeSwitch': 'Close The 4Fun Model', //
+  'setting.openSwitch': 'Open The 4Fun Model', //
+
+  'type.face': 'Face',
+  'type.eyes': 'Eyes',
+  'type.mouth': 'Mouth',
+  'type.nose': 'Nose',
+  'type.ear': 'Ear',
+  'type.eyebrows': 'Eyebrows',
+  'type.beard': 'Beard',
+  'type.hair': 'Hair',
+  'type.skinColor': 'Skin color',
+  'type.tops': 'Tops',
+  'type.bottoms': 'Bottoms',
+  'type.shones': 'Shones',
+
+  'validity.forver': 'Forver', //
+
+  'storeState.soldOut': 'SOLD OUT', //
+  'storeState.owned': 'Owned', //
+
+  'toast.openSuccess': 'Open successfully', //
+  'toast.closeSuccess': 'Closed successfully', //
+  'toast.saveSuccess': 'The current 4Fun Model has been saved', //
+  'toast.storeSoldOut':
+    'Opps~The product you selected has been sold out, and the diamonds of this product have been removed',
+};
