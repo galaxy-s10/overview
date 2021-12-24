fb258268f055730d7c07aa69ac7c70844ec3e983
shuisheng <2274751790@qq.com>
2021-12-24 14:55:11 +0800

    feat:修改脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-# 配置文件
+###
+# @Author: hss
+# @Email: 2274751790@qq.com
+# @Github: https://github.com/galaxy-s10
+# @Date: 2021-12-24
+# @Description:https://github.com/galaxy-s10/overview
+###
+ENV=$1
 echo 全局安装yarn:
 npm i yarn -g
 echo 查看yarn版本:
@@ -8,4 +15,4 @@ rm -rf node_modules
 echo 开始安装依赖：
 yarn install
 echo 执行构建命令：
-yarn run build:$1
+yarn run build:$ENV
